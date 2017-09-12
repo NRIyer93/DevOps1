cbd433f598b3af580c3ee4a148b6e115762da4f8
Naren Iyer <naren.iyer@hotmail.com>
2017-09-12 15:11:33 +0100

    Ready

--- environment/provision.sh
+++ environment/provision.sh
@@ -3,6 +3,9 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install nginx -y
+sudo rm /etc/nginx/sites-available/default
+sudo cp ~/app/environment/nginx.default /etc/nginx/sites-available/default
+sudo service nginx restart
 curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt-get install nodejs -y
