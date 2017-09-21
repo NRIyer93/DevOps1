38837deeba3ca6d512a50f4d879a4a196a3528e8
Naren Iyer <naren.iyer@hotmail.com>
2017-09-21 16:23:29 +0100

    Corrections made to main terraform file

--- devops-terraform/main.tf
+++ devops-terraform/main.tf
@@ -56,7 +56,7 @@ resource "aws_security_group" "web" {
 }
 
 resource "aws_instance" "naz-instance" {
-  ami = "${aws_ami.web.id}"
+  ami = "${data.aws_ami.web.id}"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.web.id}"]
   subnet_id = "${aws_subnet.web.id}"
